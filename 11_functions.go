29d28fb2ec31efd50a3f2740d3f67577721ab06f
alfieqashwa <alfieqashwa@gmail.com>
2017-11-12 20:49:30 +0700

    adds my exercises about golang -- 2ND COMMIT

---
+++ 11_functions.go
@@ -0,0 +1,38 @@
+// Functions are central in Go. We’ll learn about functions with a few different examples.
+
+package main
+
+import "fmt"
+
+// Here’s a function that takes two ints and returns their sum as an int.
+func plus(a int, b int) int {
+
+	// Go requires explicit returns, i.e. it won’t automatically return the value of the last expression
+	return a + b
+}
+
+/* When you have multiple consecutive parameters of the same type,
+you may omit the type name for the like-typed parameters up to the final parameter that declares the type. */
+func plusPlus(a, b, c int) int {
+	return a + b + c
+}
+
+// Call a function just as you’d expect, with name(args).
+func main() {
+
+	res := plus(1, 2)
+	fmt.Println("5+6 =", res)
+
+	res = plusPlus(4, 5, 9)
+	fmt.Println("4+5+9 =", res)
+	// see the "=", not ":=" Why??, i don't know yet.
+	//The error msg: """./functions.go:26: no new variables on left side of :="""
+}
+
+/*
+go run functions.go
+
+5+6 = 3
+4+5+9 = 18
+
+*/
---
+++ 26_select.go
@@ -0,0 +1,48 @@
+/*
+Go’s select lets you wait on multiple channel operations.
+Combining goroutines and channels with select is a powerful feature of Go. */
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	// For our example we'll select accross two channels.
+	c1 := make(chan string)
+	c2 := make(chan string)
+
+	// Each channel will receive a value after some amount of time,
+	//to simulate e.g blocking RPC operations executing in concurrent goroutines.
+	go func() {
+		time.Sleep(time.Second * 1)
+		c1 <- "one"
+	}()
+	go func() {
+		time.Sleep(time.Second * 2)
+		c1 <- "two"
+	}()
+
+	// we'll use select to await both of these values simultaneously, printing each one as it arrives.
+	for i := 0; i < 2; i++ {
+		select {
+		case msg1 := <-c1:
+			fmt.Println("receives", msg1)
+		case msg2 := <-c2:
+			fmt.Println("received", msg2)
+		}
+	}
+}
+
+/*
+We receive the values "one" and then "two" as expected.
+
+$ time go run select.go
+received one
+received two
+
+real	0m2.245s
+Note that the total execution time is only ~2 seconds since both the 1 and 2 second Sleeps execute concurrently.
+*/
