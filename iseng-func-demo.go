9297e4c7cabccea39084521d0695a819785f69c8
Alfie Qashwa <alfieqashwa@gmail.com>
2018-03-12 18:27:01 +0700

    iseng-demo-function

---
+++ iseng-func-demo.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func Add(x, y int) float32 {
+	return float32(x + y)
+}
+
+func Subtract(x, y int) float32 {
+	return float32(x - y)
+}
+
+func main() {
+	x, y := 50, 15
+	hasil := Add(x, y)
+	fmt.Printf("Hasil dari %d ditambah %d adalah %.1f\n", x, y, hasil) // Hasil dari 50 ditambah 15 adalah 65.0
+
+	hasil = Subtract(x, y)
+	fmt.Printf("Hasil dari %d dikurang %d adalah %.1f\n", x, y, hasil) // Hasil dari 50 ditambah 15 adalah 35.0
+}
