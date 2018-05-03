cc4c353ce36701545778970e5f3b92f2c3208cbb
Alfie Qashwa <alfieqashwa@gmail.com>
2018-05-03 18:45:01 +0700

    Added embedding interface example code

---
+++ interface-embedding.go
@@ -0,0 +1,48 @@
+package main
+
+import "fmt"
+
+type SalaryCalculator interface {
+	DisplaySalary()
+}
+
+type LeaveCalculator interface {
+	CalculateLeavesLeft() int
+}
+
+type EmployeeOperations interface {
+	SalaryCalculator
+	LeaveCalculator
+}
+
+type Employee struct {
+	fname       string
+	lname       string
+	basicpay    int
+	pf          int
+	totalLeaves int
+	leavesTaken int
+}
+
+func (e Employee) DisplaySalary() {
+	fmt.Printf("%s %s has salary $%d\n", e.fname, e.lname, (e.basicpay + e.pf))
+}
+
+func (e Employee) CalculateLeavesLeft() int {
+	return e.totalLeaves - e.leavesTaken
+}
+
+func main() {
+	e := Employee{
+		fname:       "Alfie",
+		lname:       "Qashwa",
+		basicpay:    5000,
+		pf:          200,
+		totalLeaves: 30,
+		leavesTaken: 5,
+	}
+	var empOp EmployeeOperations = e
+	empOp.DisplaySalary()
+
+	fmt.Println("\nLeaves left =", empOp.CalculateLeavesLeft())
+}
